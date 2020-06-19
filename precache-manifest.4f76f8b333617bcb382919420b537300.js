1c69493b66be94d7cb0d1422f8eec07b0b78b241
Paweł Tylczyński <pawel.tylczynski@gmail.com>
2020-06-19 15:49:47 +0200

    Updates

---
+++ precache-manifest.4f76f8b333617bcb382919420b537300.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f2531f26b401526e5edd6ff8c1c27f",
+    "url": "/pt-player-task/index.html"
+  },
+  {
+    "revision": "42a3809f0c3fcafdba85",
+    "url": "/pt-player-task/static/css/main.95d0f915.chunk.css"
+  },
+  {
+    "revision": "05d660f20deed3c2dc03",
+    "url": "/pt-player-task/static/js/2.d1e4d18f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pt-player-task/static/js/2.d1e4d18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a3809f0c3fcafdba85",
+    "url": "/pt-player-task/static/js/main.087320bf.chunk.js"
+  },
+  {
+    "revision": "9fc9b5716dc1710c336e",
+    "url": "/pt-player-task/static/js/runtime-main.0a915d02.js"
+  },
+  {
+    "revision": "d522478d9c40ce1d72360effa1c66521",
+    "url": "/pt-player-task/static/media/Play_active.d522478d.png"
+  },
+  {
+    "revision": "52d51b7d5b834b033266df7aeaa497b9",
+    "url": "/pt-player-task/static/media/bg_image.52d51b7d.jpg"
+  },
+  {
+    "revision": "a21957684324528264df44116fc5c48c",
+    "url": "/pt-player-task/static/media/unreleased_cover.a2195768.png"
+  }
+]);
